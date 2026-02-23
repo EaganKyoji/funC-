082536f6304ed1821343f7f0abc59c467559c688
Wind <rainmahasin2000@gmail.com>
2026-02-23 14:13:33 +0700

    Alghoritm fixed and UI Upgrade

--- caesarChiper.cpp
+++ caesarChiper.cpp
@@ -1,24 +1,57 @@
 #include <iostream>
+#include <iomanip>
 using namespace std;
 
+void judul();
+void closed();
+
 int main(){
+    judul();
+
     string text;
     int geser;
 
-    cout<<"Masukkan teks: ";
+    cout<<left<<setw(12)<<"Plain text"<<":";
     getline(cin, text);
     
-    cout<<"Masukkan pergeseran: ";
+    cout<<left<<setw(12)<<"Pergeseran"<<":";
     cin>>geser;
     
     int length = text.length();
     int i = 0;
 
     while(i<length){
-        text[i] = text[i]+geser;
+        if(text[i]>='a' && text[i]<='z'){
+            text[i] = (text[i] - 'a' + geser)%26 + 'a';
+        }
+        else if(text[i]>='A' && text[i]<='Z'){
+            text[i] = (text[i] - 'A' + geser)%26 + 'A';
+        }
+        else{
+            text = "GALAT! STRING/CHAR TIDAK DIDETEKSI";
+        }
         i++;
     }
 
-    cout<<text;
+    closed();
+    cout<<left<<setw(12)<<"Chiper text"<<":"<<text<<"\n";
+    closed();
 
+}
+
+void judul(){
+    cout<<"█████████████████████████████████████████████████████\n";
+    cout<<"█─▄▄▄─██▀▄─██▄─▄▄─█─▄▄▄▄██▀▄─██▄─▄▄▀█████████████████\n";
+    cout<<"█─███▀██─▀─███─▄█▀█▄▄▄▄─██─▀─███─▄─▄█████████████████\n";
+    cout<<"▀▄▄▄▄▄▀▄▄▀▄▄▀▄▄▄▄▄▀▄▄▄▄▄▀▄▄▀▄▄▀▄▄▀▄▄▀▀▀██████████████\n";
+    cout<<"█████████████████████████████████████████████████████\n";
+    cout<<"█████████████████████─▄▄▄─█─█─█▄─▄█▄─▄▄─█▄─▄▄─█▄─▄▄▀█\n";
+    cout<<"█████████████████████─███▀█─▄─██─███─▄▄▄██─▄█▀██─▄─▄█\n";
+    cout<<"████████████████████▀▄▄▄▄▄▀▄▀▄▀▄▄▄▀▄▄▄▀▀▀▄▄▄▄▄▀▄▄▀▄▄▀\n";
+    cout<<"█████████████████████████████████████████████████████\n";
+    cout<<"╔════════════════════════════════════════════════════╗\n";
+}
+
+void closed(){
+    cout<<"╚════════════════════════════════════════════════════╝\n";
 }
